6a45d591211448dbbd523647984ae73d9d00daa4
shamerdanov <shamil332211@list.ru>
2024-06-21 01:20:27 +0400

    init

--- app/src/main/java/com/example/museumsinturkey/data/api/ApiService.kt
+++ app/src/main/java/com/example/museumsinturkey/data/api/ApiService.kt
@@ -1,4 +1,25 @@
 package com.example.museumsinturkey.data.api
 
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiService {
+
+    @GET("service/museum/cities")
+    suspend fun getAllCity(@Query("apiKey") apiKey: String = "gvloSjuIbqB3u0EDg5OrFcIOG2ac74gAgi46NVEYweHnNfaEcF6yajYAxC8M")
+            : Response<CityResponse>
+
+    @GET("service/museum/cities")
+    suspend fun getAllDistrict(
+        @Query("apiKey") apiKey: String = "gvloSjuIbqB3u0EDg5OrFcIOG2ac74gAgi46NVEYweHnNfaEcF6yajYAxC8M",
+        @Query("city") city: String
+    ): Response<DistrictResponse>
+
+    @GET("service/museum")
+    suspend fun getMuseumByCityAndDistrict(
+        @Query("apiKey") apiKey: String = "gvloSjuIbqB3u0EDg5OrFcIOG2ac74gAgi46NVEYweHnNfaEcF6yajYAxC8M",
+        @Query("city") city: String,
+        @Query("district") district: String
+    ): Response<MuseumResponse>
 }
