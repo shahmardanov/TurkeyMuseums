47074a5d844d539a0dbec0b38fb3ce360edcc8a9
shamerdanov <shamil332211@list.ru>
2024-06-21 01:16:20 +0400

    init

--- app/src/main/java/com/example/museumsinturkey/utill/Constant.kt
+++ app/src/main/java/com/example/museumsinturkey/utill/Constant.kt
@@ -2,5 +2,5 @@ package com.example.museumsinturkey.utill
 
 object Constant {
     const val BASE_URL = "https://www.nosyapi.com/"
-    const val API_PREFIX = "apiv2/"
+    const val API_VERSION = "apiv2/"
 }
