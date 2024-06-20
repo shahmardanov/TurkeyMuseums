a4788734902931d1e4d7df74924f06e1e8f91bb6
shamerdanov <shamil332211@list.ru>
2024-06-21 01:14:14 +0400

    init

--- app/src/main/java/com/example/museumsinturkey/di/NetworkModule.kt
+++ app/src/main/java/com/example/museumsinturkey/di/NetworkModule.kt
@@ -1,4 +1,30 @@
 package com.example.museumsinturkey.di
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constant.BASE_URL + Constant.API_PREFIX)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideAPIServices(retrofit: Retrofit): APIServices {
+        return retrofit.create(APIServices::class.java)
+    }
+
 }
